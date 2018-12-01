d0c0c58de558c0db9a24bed8f63daac8ceb157d8
Caleb <ande4905@gmail.com>
2018-11-30 18:34:07 -0600

    all the changes
    I added the map page, and I also added the javascript Nav menu!

---
+++ js/map.js
@@ -0,0 +1,124 @@
+﻿var map;
+var geocoder;
+var marker;
+var locations = [];
+locations = JSON.parse(window.localStorage.getItem("treeData"));
+
+function initMap() {
+    var labels = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    geocoder = new google.maps.Geocoder();
+    var latlng = new google.maps.LatLng(44.945706, -93.182121);
+    var mapOptions = {
+        zoom: 8,
+        center: latlng
+    }    
+    map = new google.maps.Map(document.getElementById('map'), mapOptions);
+    if (locations != null && locations.length > 0)
+    var markers = locations.map(function (location, i) {
+        return new google.maps.Marker({
+            position: location,
+            label: labels[i % labels.length]
+        });
+    });
+    var markerCluster = new MarkerClusterer(map, markers,
+        { imagePath: 'images/m' });
+
+    map.addListener('click', function (e) {
+        placeMarkerAndPanTo(e.latLng, map);
+    });
+    map.addListener('center_changed', function () {
+        window.setTimeout(function () {
+            if (marker != undefined)
+            map.panTo(marker.getPosition());
+        }, 4500);
+    });
+}
+
+function placeMarkerAndPanTo(latLng, map) {
+    if (marker != null) {
+        marker.setPosition(latLng);
+    } else {
+    var image = "images/tree icon.png";
+    marker = new google.maps.Marker({
+        map: map,
+        draggable: true,
+        animation: google.maps.Animation.DROP,
+        position: latLng,
+        title: "Drag me!",
+        icon: image
+        });
+    }
+    map.panTo(latLng);
+    marker.addListener('click', function () {
+        map.setZoom(13);
+        map.setCenter(marker.getPosition());
+    });
+}
+
+function codeAddress() {
+    var address = document.getElementById('address').value;
+    geocoder.geocode({'address': address}, function (results, status) {
+        if (status == 'OK') {
+            map.setCenter(results[0].geometry.location);
+            map.setZoom(12);
+            if (marker != null) {
+                marker.setPosition(results[0].geometry.location);
+            } else {
+                var image = "images/tree icon.png";
+                marker = new google.maps.Marker({
+                    map: map,
+                    draggable: true,
+                    animation: google.maps.Animation.DROP,
+                    position: results[0].geometry.location,
+                    title: "Drag me!",
+                    icon: image
+                });
+            }
+        } else {
+            alert('Geocode was not successful for the following reason: ' + status);
+        }
+        marker.addListener('click', function () {
+            map.setZoom(13);
+            map.setCenter(marker.getPosition());
+        });
+    });
+}
+
+function recordMarker() {
+    var mPoint = marker.getPosition();
+    var mPoints = [];
+    mPoints.push(mPoint);
+    var storage = window.localStorage.getItem("treeData");
+    if (storage != null && storage.length > 0) {
+        var arr = JSON.parse(storage);
+        arr.push(mPoint);
+        window.localStorage.setItem("treeData", JSON.stringify(arr));
+    } else {
+        window.localStorage.setItem("treeData", JSON.stringify(mPoints));
+    }    
+    alert("Location Recorded")
+    location.reload();
+}
+
+function deleteMarkers() {
+    locations = [];
+    window.localStorage.setItem("treeData", JSON.stringify(locations));
+    location.reload();
+}
+
+function stateView() {
+    map.setZoom(7);
+    if (marker != undefined)
+    map.setCenter(marker.getPosition());
+}
+
+function cityView() {
+    map.setZoom(11);
+    if (marker != undefined)
+    map.setCenter(marker.getPosition());
+}
+
+//function markersView() {
+//    var latlng = locations;
+//    map.setCenter(latlng);
+//}
--- js/script.js
+++ js/script.js
@@ -1,1 +1,45 @@
+function openNav() {
+    document.getElementById("websiteNav").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "260px";
+    document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+}
 
+function closeNav() {
+    document.getElementById("websiteNav").style.width = "0";
+    document.getElementById("main").style.marginLeft= "10%";
+    document.body.style.backgroundColor = "white";
+}
+
+// function uploadFile() {
+//     // get the file as a string
+//     var formData = new FormData($("#first")[0]);
+//     var xhr = new XMLHttpRequest();
+//     xhr.addEventListener("load", transferComplete, false);
+//     xhr.addEventListener("error", transferFailed, false);
+//     xhr.addEventListener("abort", transferCanceled, false);
+//     xhr.open('POST', 'upload.php', true);
+//     xhr.send(formData); 		     // actually send the form data
+//     function transferComplete(evt) {     // stylized upload complete
+//         $("#progress").css("width", "100%");
+//         $("#progress").html("100%");
+//     }
+//     function transferFailed(evt) {
+//         alert("An error occurred while transferring the file.");
+//     }
+//     function transferCanceled(evt) {
+//         alert("The transfer has been canceled by the user.");
+//     }
+// }
+
+// //single file
+// var xhr = new XMLHttpRequest();
+// // reference to the 1st file input field
+// var theFile = $(":file")[0].files[0];
+// var formData = new FormData();
+// formData.append('images', theFile);
+
+// //multiple files
+// var allFiles = $(":file")[0].files;
+// for (var i = 0; i < allFiles.length; i++) {
+//     formData.append('images[]', allFiles[i]);
+// }
